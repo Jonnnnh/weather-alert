bb5d9555ec975210109ebfbd0f6fe2df8e79cb2d
sinemoraa <jonshilin.gg@gmail.com>
2025-01-13 01:16:50 +0300

    feat: add data migration service

---
+++ data-migration-service/src/test/java/com/example/notificationservice/datamigrationservice/DataMigrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationservice.datamigrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
