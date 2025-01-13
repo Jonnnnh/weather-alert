51005bd1445429395557c8022c9765dd369075ea
sinemoraa <jonshilin.gg@gmail.com>
2025-01-14 01:49:01 +0300

    feat: add Preferences and User entity

---
+++ models/src/main/java/com/example/models/entities/Preferences.java
@@ -0,0 +1,53 @@
+package com.example.models.entities;
+
+import com.example.models.enums.Frequency;
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "preferences")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Preferences {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "city", nullable = false, length = 100)
+    private String city;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "frequency", nullable = false, length = 20)
+    private Frequency frequency;
+
+    @Column(name = "alerts", columnDefinition = "jsonb", nullable = false)
+    private String alerts;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
+
---
+++ models/src/main/java/com/example/models/entities/User.java
@@ -0,0 +1,41 @@
+package com.example.models.entities;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "telegram_id", unique = true, nullable = false, length = 50)
+    private String telegramId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
