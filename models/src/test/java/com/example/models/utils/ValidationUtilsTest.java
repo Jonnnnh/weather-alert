9dce708838132e7ce10b19cb2510fd2447f2a5f5
sinemoraa <jonshilin.gg@gmail.com>
2025-01-15 21:06:46 +0300

    test(utils): add tests for isValidFrequency in ValidationUtils

--- models/src/test/java/com/example/models/utils/ValidationUtilsTest.java
+++ models/src/test/java/com/example/models/utils/ValidationUtilsTest.java
@@ -13,4 +13,20 @@ public class ValidationUtilsTest {
         assertFalse(ValidationUtils.isValidTelegramId(""));
         assertFalse(ValidationUtils.isValidTelegramId("a".repeat(51)));
     }
+
+    @Test
+    public void testIsValidFrequency() {
+        assertTrue(ValidationUtils.isValidFrequency("DAILY"));
+        assertTrue(ValidationUtils.isValidFrequency("HOURLY"));
+        assertTrue(ValidationUtils.isValidFrequency("WEEKLY"));
+
+        assertTrue(ValidationUtils.isValidFrequency("daily"));
+        assertTrue(ValidationUtils.isValidFrequency("Hourly"));
+        assertTrue(ValidationUtils.isValidFrequency("WeEkLy"));
+
+        assertFalse(ValidationUtils.isValidFrequency("MONTHLY"));
+        assertFalse(ValidationUtils.isValidFrequency(null));
+        assertFalse(ValidationUtils.isValidFrequency(""));
+        assertFalse(ValidationUtils.isValidFrequency("random"));
+    }
 }
