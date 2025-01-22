66f7e9afdac41a58f96332a7e76fa346c459cf9b
sinemoraa <jonshilin.gg@gmail.com>
2025-01-23 01:55:23 +0300

    refactor: simplify interface by removing userDTO parameter

--- bot/src/main/java/com/example/bot/telegram/commands/Command.java
+++ bot/src/main/java/com/example/bot/telegram/commands/Command.java
@@ -1,9 +1,8 @@
 package com.example.bot.telegram.commands;
 
-import com.example.models.dto.UserDTO;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 
 public interface Command {
-    SendMessage handle(UserDTO userDTO , Update update);
+    SendMessage handle(Update update);
 }
