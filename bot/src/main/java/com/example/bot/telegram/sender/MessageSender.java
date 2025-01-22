5c3231eb0266d16beb77fd5bde7edb6936bf2420
sinemoraa <jonshilin.gg@gmail.com>
2025-01-23 01:56:36 +0300

    refactor: update interface to return void for sendMessage method

--- bot/src/main/java/com/example/bot/telegram/sender/MessageSender.java
+++ bot/src/main/java/com/example/bot/telegram/sender/MessageSender.java
@@ -1,9 +1,7 @@
 package com.example.bot.telegram.sender;
 
-
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 
 public interface MessageSender {
-    SendResponse sendMessage(SendMessage sendMessage);
+    void sendMessage(SendMessage sendMessage);
 }
