0813877104d607102cef17cc1c07a7caf1f29449
Dasha <jonshilin.gg@gmail.com>
2025-01-24 03:14:26 +0300

    test: specify configuration class for spring boot test in DataMigrationServiceApplicationTests

--- data-migration-service/src/test/java/com/example/notificationservice/datamigrationservice/DataMigrationServiceApplicationTests.java
+++ data-migration-service/src/test/java/com/example/notificationservice/datamigrationservice/DataMigrationServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.notificationservice.datamigrationservice;
 
+import com.example.datamigrationservice.DataMigrationServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DataMigrationServiceApplication.class)
 class DataMigrationServiceApplicationTests {
 
 	@Test
