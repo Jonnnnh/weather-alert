4f9a614ef2e6265cb9a085e439aa2cce1e17ca98
Dasha <jonshilin.gg@gmail.com>
2025-01-24 03:18:24 +0300

    refactor: update to use CommandManager for command handling

--- bot/src/main/java/com/example/bot/telegram/handler/UpdateHandlerImpl.java
+++ bot/src/main/java/com/example/bot/telegram/handler/UpdateHandlerImpl.java
@@ -1,7 +1,6 @@
 package com.example.bot.telegram.handler;
 
-import com.example.bot.telegram.commands.HelpCommand;
-import com.example.bot.telegram.reply.ReplyMessages;
+import com.example.bot.telegram.commands.commandManager.CommandManager;
 import com.example.bot.telegram.sender.MessageSender;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
@@ -11,20 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UpdateHandlerImpl implements UpdateHandler {
-    private final HelpCommand helpCommand;
+    private final CommandManager commandManager;
     private final MessageSender messageSender;
 
     @Override
     public void handleUpdate(Update update) {
         if (update.message() != null && update.message().text() != null) {
-            String text = update.message().text().toLowerCase();
-            if (text.equals("/help")) {
-                messageSender.sendMessage(helpCommand.handle(update));
-            } else {
-                String chatId = update.message().chat().id().toString();
-                var response = new SendMessage(chatId, ReplyMessages.UNKNOWN_COMMAND.getMessage());
-                messageSender.sendMessage(response);
-            }
+            SendMessage response = commandManager.handleCommand(update);
+            messageSender.sendMessage(response);
         }
     }
 }
