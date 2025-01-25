f536bcd6553f1193e78841d8f607a61bffa21f90
Dasha <jonshilin.gg@gmail.com>
2025-01-25 03:41:31 +0300

    feat: update database schema

--- data-migration-service/src/main/resources/db/db.changelog-1.0.sql
+++ data-migration-service/src/main/resources/db/db.changelog-1.0.sql
@@ -10,8 +10,12 @@ CREATE TABLE users (
 
 CREATE TABLE weather_data (
                               id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                              user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-                              content JSONB NOT NULL,
+                              city VARCHAR(100) NOT NULL,
+                              temperature DOUBLE PRECISION,
+                              cloudiness INTEGER,
+                              humidity INTEGER,
+                              condition VARCHAR(100),
+                              is_day BOOLEAN,
                               recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
