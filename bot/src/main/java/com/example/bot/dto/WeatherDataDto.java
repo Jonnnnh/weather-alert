000bd68a21f1b8cce73d75b38dd09f7c5c02ece5
Dasha <jonshilin.gg@gmail.com>
2025-01-31 14:39:56 +0300

    chore: duplicate class from weather-service for bot service

---
+++ bot/src/main/java/com/example/bot/dto/WeatherDataDto.java
@@ -0,0 +1,20 @@
+package com.example.bot.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherDataDto {
+    private String city;
+    private Double temperature;
+    private Integer cloudiness;
+    private Integer humidity;
+    private String condition;
+    private Boolean isDay;
+    private LocalDateTime recordedAt;
+}
