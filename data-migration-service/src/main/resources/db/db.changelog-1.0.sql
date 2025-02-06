747af7156aee7e6d88b767d35d54d1fb72594fef
Dasha <jonshilin.gg@gmail.com>
2025-02-06 21:26:28 +0300

    feat^ add new column in table weather_data

--- data-migration-service/src/main/resources/db/db.changelog-1.0.sql
+++ data-migration-service/src/main/resources/db/db.changelog-1.0.sql
@@ -10,6 +10,7 @@ CREATE TABLE users (
 
 CREATE TABLE weather_data (
                               id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                              chat_id VARCHAR(50) NOT NULL,
                               city VARCHAR(100) NOT NULL,
                               temperature DOUBLE PRECISION,
                               cloudiness INTEGER,
