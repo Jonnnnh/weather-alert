7fb6e83939e8ad816a884713fe8fc0d5dc05f239
Dasha <jonshilin.gg@gmail.com>
2025-02-08 21:16:32 +0300

    feat: add exception for error handling

--- weather-service/src/main/java/com/example/weatherservice/client/CurrentWeatherApiClientImpl.java
+++ weather-service/src/main/java/com/example/weatherservice/client/CurrentWeatherApiClientImpl.java
@@ -1,13 +1,12 @@
 package com.example.weatherservice.client;
 
+import com.example.weatherservice.exception.WeatherServiceInternalException;
 import com.example.weatherservice.util.WeatherApiUrlBuilderImpl;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CurrentWeatherApiClientImpl implements CurrentWeatherApiClient {
@@ -17,15 +16,11 @@ public class CurrentWeatherApiClientImpl implements CurrentWeatherApiClient {
 
     @Override
     public String fetchCurrentWeather(String city) {
-        log.info("Запрашиваем данные о погоде для города: {}", city);
-
         String url = urlBuilder.buildUrl(city);
-
         try {
             return restTemplate.getForObject(url, String.class);
         } catch (Exception e) {
-            log.error("Ошибка при вызове Weather API", e);
-            throw new RuntimeException("Failed to fetch weather data", e);
+            throw new WeatherServiceInternalException("Failed to fetch weather data", e);
         }
     }
 }
