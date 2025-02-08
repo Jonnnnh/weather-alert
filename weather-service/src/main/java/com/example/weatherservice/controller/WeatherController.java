23f48ab3378f75cefc821dab296256ba8577946b
Dasha <jonshilin.gg@gmail.com>
2025-02-08 21:17:16 +0300

    refactor: remove @Slf4j

--- weather-service/src/main/java/com/example/weatherservice/controller/WeatherController.java
+++ weather-service/src/main/java/com/example/weatherservice/controller/WeatherController.java
@@ -3,10 +3,8 @@ package com.example.weatherservice.controller;
 import com.example.weatherservice.dto.WeatherDataDto;
 import com.example.weatherservice.service.FetchWeather;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/weather")
 @RequiredArgsConstructor
--- weather-service/src/main/java/com/example/weatherservice/service/FetchWeatherImpl.java
+++ weather-service/src/main/java/com/example/weatherservice/service/FetchWeatherImpl.java
@@ -2,10 +2,8 @@ package com.example.weatherservice.service;
 
 import com.example.weatherservice.dto.WeatherDataDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class FetchWeatherImpl implements FetchWeather {
