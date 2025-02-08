0cc821881e45e3938a3e589e2f77fca59204dc6b
Dasha <jonshilin.gg@gmail.com>
2025-02-08 14:43:16 +0300

    chore: enable configuration properties

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,9 +1,12 @@
 package com.example.userservice;
 
+import com.example.userservice.configuration.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
