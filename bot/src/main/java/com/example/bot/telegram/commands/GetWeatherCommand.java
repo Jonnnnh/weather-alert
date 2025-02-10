f5477bae9b453294231aea8fc852ca6886cb8ae7
Dasha <jonshilin.gg@gmail.com>
2025-02-10 14:35:22 +0300

    refactor: update GetWeatherCommand to use gRPC and internal services

--- bot/src/main/java/com/example/bot/telegram/commands/GetWeatherCommand.java
+++ bot/src/main/java/com/example/bot/telegram/commands/GetWeatherCommand.java
@@ -1,52 +1,43 @@
 package com.example.bot.telegram.commands;
 
-import com.example.bot.clients.UserServiceClient;
-import com.example.bot.clients.WeatherServiceClient;
-import com.example.bot.dto.UserDTO;
-import com.example.bot.dto.WeatherDataDto;
-import com.example.bot.telegram.reply.ReplyMessages;
+import com.example.bot.clients.GrpcUserServiceClient;
+import com.example.bot.service.WeatherRequestSender;
+import com.example.bot.service.storage.UserRequestStorage;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class GetWeatherCommand implements Command {
 
-    private final UserServiceClient userServiceClient;
-    private final WeatherServiceClient weatherServiceClient;
+    private final GrpcUserServiceClient grpcUserServiceClient;
+    private final WeatherRequestSender weatherRequestSender;
+    private final UserRequestStorage userRequestStorage;
 
     @Override
     public SendMessage handle(Update update) {
         String chatId = String.valueOf(update.message().chat().id());
-        Optional<UserDTO> userOpt = userServiceClient.getUserByTelegramId(chatId);
+        var userOpt = grpcUserServiceClient.getUserByTelegramId(chatId);
 
         if (userOpt.isPresent()) {
-            UserDTO user = userOpt.get();
+            var user = userOpt.get();
             String city = user.getCity();
 
-            if (city != null && !city.isEmpty()) {
-                WeatherDataDto weatherData = weatherServiceClient.fetchCurrentWeather(city);
-
-                if (weatherData != null) {
-                    return new SendMessage(chatId, "Погода в городе " + city + ":\n" +
-                            "Температура: " + weatherData.getTemperature() + "°C\n" +
-                            "Облачность: " + weatherData.getCloudiness() + "%\n" +
-                            "Влажность: " + weatherData.getHumidity() + "%\n" +
-                            " " + weatherData.getCondition());
-                } else {
-                    return new SendMessage(chatId, ReplyMessages.WEATHER_DATA_ERROR.getMessage());
-                }
+            if (!city.isEmpty()) {
+                userRequestStorage.storeTelegramId(city, chatId);
+                weatherRequestSender.sendWeatherRequest(chatId, city);
+                return new SendMessage(chatId, "Запрос на получение погоды отправлен, ожидайте...");
             } else {
-                return new SendMessage(chatId, ReplyMessages.CITY_NOT_SET.getMessage());
+                return new SendMessage(chatId, "У вас не задан город. Используйте команду /setcity <город>");
             }
         } else {
-            return new SendMessage(chatId, ReplyMessages.USER_NOT_FOUND.getMessage());
+            return new SendMessage(chatId, "Пользователь не найден. Сначала введите /start, чтобы зарегистрироваться");
         }
     }
 }
+
+
