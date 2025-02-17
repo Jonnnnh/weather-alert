2da7a2857f9c9ef609e4aadfe6f76743e66375da
Dasha <jonshilin.gg@gmail.com>
2025-02-17 15:54:23 +0300

    feat: add initial liquibase changelog and weather table creation

--- weather-service/src/main/resources/db/db.changelog-1.0.sql
+++ weather-service/src/main/resources/db/db.changelog-1.0.sql
@@ -1,13 +1,3 @@
-CREATE TABLE users (
-                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                       telegram_id VARCHAR(50) UNIQUE NOT NULL,
-                       city VARCHAR(100) NOT NULL,
-                       frequency VARCHAR(20) NOT NULL,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-
 CREATE TABLE weather_data (
                               id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                               city VARCHAR(100) NOT NULL,
